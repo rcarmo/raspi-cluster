43be48c4fe496316a6532110c41bb1140f6fa6e5
Rui Carmo <rui.carmo@gmail.com>
2014-04-28 23:05:58 +0100

    cleanup

--- cluster-monitor/test/cluster_monitor/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns cluster-monitor.core-test
-  (:require [clojure.test :refer :all]
-            [cluster-monitor.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
